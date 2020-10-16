80ad966620aa75f35d16e625c954cd4b78b796b2
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-10-16 20:10:55 +0700

    IF1-10119011-Latihan4-Kambing

---
+++ src/if1/pkg10119011/latihan4/kambing/Kambing.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ *  Kelas   : IF-1
+ *  Nim     : 10119011
+ */
+public class Kambing {
+    public void tambahKambing() {
+        //Deklarasi Kambing
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing Setelah Ditambah: " + jumlahKambing);
+    }
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
